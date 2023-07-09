b2e1a8ade5986620f73a1d086dd6e3849fd65239
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-09 22:46:15 +0200

    .

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,24 @@
 import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("<App />", () => {
-  test("App mounts properly", () => {
-    const wrapper = render(<App />);
-    expect(wrapper).toBeTruthy();
+  const wrapper = render(<App />);
 
+  test("App mounts h1 properly", () => {
     // Get by h1
     const h1 = wrapper.container.querySelector("h1");
     expect(h1?.textContent).toBe("Customer Acquisition Channel CRUD");
+  });
+  test("App mounts h2 properly", () => {
+    const wrapper = render(<App />);
 
     // Get by h2
     const h2 = wrapper.container.querySelector("h2");
     expect(h2?.textContent).toBe("Table");
+  });
+  test("App mounts table properly", () => {
+    const wrapper = render(<App />);
 
     // Get by Table
     const table = wrapper.container.querySelector("table");
